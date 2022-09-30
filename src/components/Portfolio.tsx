3e65511804d5bb00376f3ffcd75f05d08e4d520b
Yuriy Zvirblis <ogwhitiez@gmail.com>
2022-09-30 20:23:41 +0300

    comment

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,32 +1,32 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+// import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+// import installNode from "../assets/portfolio/installNode.jpg";
+// import navbar from "../assets/portfolio/navbar.jpg";
+// import reactParallax from "../assets/portfolio/reactParallax.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 function Portfolio() {
-  const portfolios = [
-    {
-      src: arrayDestruct,
-    },
-    {
-      src: reactParallax,
-    },
-    {
-      src: navbar,
-    },
-    {
-      src: reactSmooth,
-    },
-    {
-      src: installNode,
-    },
-    {
-      src: reactWeather,
-    },
-  ];
+  // const portfolios = [
+  //   {
+  //     src: arrayDestruct,
+  //   },
+  //   {
+  //     src: reactParallax,
+  //   },
+  //   {
+  //     src: navbar,
+  //   },
+  //   {
+  //     src: reactSmooth,
+  //   },
+  //   {
+  //     src: installNode,
+  //   },
+  //   {
+  //     src: reactWeather,
+  //   },
+  // ];
 
   return (
     <div
